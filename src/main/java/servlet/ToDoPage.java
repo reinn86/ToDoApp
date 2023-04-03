20cbfaeccb9619616a0012f44ab91f32ad1d2ca5
haruki takahashi <tanukichang@outlook.jp>
2023-04-04 06:17:13 +0900

    タスク修正機能の仮作成

--- src/main/java/servlet/ToDoPage.java
+++ src/main/java/servlet/ToDoPage.java
@@ -120,18 +120,17 @@ public class ToDoPage extends HttpServlet {
 			}
 			
 			taskListHtml = taskListHtml 
-				+ "<form action=\"\" method=\"post\">"
 				+ "<li class=index"+i+">"
 //				+ modifyHtml
+				+ "<form action=\"\" method=\"post\">"
 				+ "<p id=\"process\" style=\"display:inline;\">" + contents + " </p>"
 				+ "<small>" + term + " </small>"
 				+ "<input type=\"submit\" value=\"完了\" name=\"COMPLETE\">"
 				+ "<input type=\"submit\" value=\"削除\" name=\"DELETE\">"
-				+ "<input type=\"submit\" value=\"修正\" name =\"MODIFICATION\">"
+//				+ "<input type=\"submit\" value=\"修正\" name =\"MODIFICATION\">"
 				+ "<input type=\"hidden\" value=\"" + i + "\" name=\"index\">"
-				+ "</li>"
-				+ "</form>";
-
+				+ "</form>"
+				+ "</li>";
 		}
 		//ページ切り替え
 		request.setAttribute("task",taskListHtml);
--- src/main/webapp/assets/js/main.js
+++ src/main/webapp/assets/js/main.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function process() {
+	let contentsText = document.getElementById('process').innerText
+	
 	document.getElementById('process')
-		.innerHTML = "<input type=\"text\" hint=\"aaaa\"></input>";
+		.outerHTML = '<input type=\"text\" value=\"' + contentsText + '\" name=\"MODIFICATION\"></input>';
 }
 
 document.getElementById('process').addEventListener('click', process);
+document.getElementById('process').addEventListener('content_modification', process);
